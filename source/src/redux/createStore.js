4fe0c514a7ae1de10822411862f4a05ad2b1f197
dengbupapapa <meizhanghao1989@qq.com>
2017-05-25 14:01:38 +0800

    createStore correction push redux middleware

--- source/src/redux/createStore.js
+++ source/src/redux/createStore.js
@@ -17,23 +17,17 @@ if (process.env.NODE_ENV == 'development') {
     applyMiddlewares.push(log);
 }
 
-const enhancer = compose(
-    //你要使用的中间件，放在前面
-    applyMiddleware(...applyMiddlewares),
-    //必须的！启用带有monitors（监视显示）的DevTools
-    DevTools.instrument()
+let composes = [applyMiddleware(...applyMiddlewares)];
 
-)
+if (process.env.NODE_ENV == 'development') {
+    composes.push(DevTools.instrument());
+}
+
+const enhancer = compose(...composes);
 
 export default initialState => {
     //注意：仅仅只有redux>=3.1.0支持第三个参数
-    let store;
-
-    if (process.env.NODE_ENV == 'development') {
-        store = createStore(rootReducer, initialState, enhancer);
-    } else {
-        store = createStore(rootReducer, initialState);
-    }
+    let store = createStore(rootReducer, initialState, enhancer);
 
     //热替换选项
     if (module.hot) {
